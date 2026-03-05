4506cbb798a6c6bea0b1a3fd0f899c6219ad0629
Yudha <yudhasanggrama@gmail.com>
2026-03-05 12:46:31 +0700

    update AppProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,16 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa semua asset (CSS/JS) pakai HTTPS di server
+        if (config('app.env') === 'production' || $this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
